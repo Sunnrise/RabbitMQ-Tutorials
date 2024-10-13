ad898644cb5a1569d1429cca92febaceb3a1b6a9
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-13 20:46:36 +0300

    basic consumer and publisher application

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+//Connection initialize
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vbqelytc:MbF26kun1-5hNwzQ9Q8OQ--HckTNQuZu@shark.rmq.cloudamqp.com/vbqelytc");
+
+
+//Active connection and open channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//Queue Creation
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+
+//Get message from queue
+//RabbitMQ carry messages as byte type. So we have to convert byte to actual type
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue",false,consumer);
+consumer.Received += (sender, e) =>
+{//e.body give us whole message as byte
+ //e.body.Span or e.body.ToArray() can be used to convert byte to actual type
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+//Connection initialize
+ConnectionFactory factory = new();
+factory.Uri= new("amqps://vbqelytc:MbF26kun1-5hNwzQ9Q8OQ--HckTNQuZu@shark.rmq.cloudamqp.com/vbqelytc");
+
+
+//Active connection and open channel
+using IConnection connection =factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//Queue Creation
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+
+//Send message to queue
+//RabbitMQ accept messages as byte type. So we have to convert data to byte. 
+
+for(int i=0;i<100;i++)
+{   
+    await Task.Delay(500);
+    byte[] message=Encoding.UTF8.GetBytes("Helloworld "+i);
+    channel.BasicPublish(exchange:"", routingKey:"example-queue", body: message);
+}
+Console.Read();
