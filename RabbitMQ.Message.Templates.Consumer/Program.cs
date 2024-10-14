4c0f156361ff4de50b17a14754c58438fd303740
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-14 13:11:21 +0300

    Message design types: p2p, pub-sub, work queue, request-response

---
+++ RabbitMQ.Message.Templates.Consumer/Program.cs
@@ -0,0 +1,122 @@
+﻿
+
+//Connection initialize
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vbqelytc:MbF26kun1-5hNwzQ9Q8OQ--HckTNQuZu@shark.rmq.cloudamqp.com/vbqelytc");
+
+//Active connection and open channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+#region P2P, Point to Point design
+//string queueName = "Example-p2p-queue";
+
+//channel.QueueDeclare(
+//    queue: queueName,
+//    durable: false,
+//    exclusive: false,
+//    autoDelete: false);
+
+//EventingBasicConsumer consumer = new(channel);
+
+//channel.BasicConsume(
+//    queue: queueName,
+//    autoAck: false,
+//    consumer: consumer);
+
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+
+#endregion
+
+#region Pub/Sub, Publish/Subscribe design 
+//string exchangeName = "example-pub-sub-exchange";
+
+//channel.ExchangeDeclare(
+//    exchange: exchangeName,
+//    type: ExchangeType.Fanout);
+
+//string queueName = channel.QueueDeclare().QueueName;
+//channel.QueueBind(
+//    queue: queueName,
+//    exchange: exchangeName,
+//    routingKey: string.Empty);
+
+//EventingBasicConsumer consumer = new(channel);
+//channel.BasicConsume(
+//    queue: queueName,
+//    autoAck: false,
+//    consumer: consumer);
+
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+#endregion
+
+#region Work Queues design
+//string queueName = "example-work-queue";
+
+//channel.QueueDeclare(
+//    queue: queueName,
+//    durable: false,
+//    exclusive: false,
+//    autoDelete: false);
+
+//EventingBasicConsumer consumer = new(channel);
+//channel.BasicConsume(
+//    queue: queueName,
+//    autoAck: true,
+//    consumer: consumer);
+
+//channel.BasicQos(
+//    prefetchSize: 1,
+//    prefetchCount: 0,
+//    global: false);
+
+//consumer.Received += (sender, e) =>
+//{
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//};
+#endregion
+
+#region Request/Response design
+string requestQueueName = "example-request-response-queue";
+
+channel.QueueDeclare(
+    queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: requestQueueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    string message = Encoding.UTF8.GetString(eventArgs.Body.Span);
+    Console.WriteLine(message);
+
+    byte[] responseMessage = Encoding.UTF8.GetBytes($" Process Done: {message} ");
+
+    IBasicProperties properties = channel.CreateBasicProperties();
+    properties.CorrelationId = eventArgs.BasicProperties.CorrelationId;
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: eventArgs.BasicProperties.ReplyTo,
+        basicProperties: properties,
+        body: responseMessage);
+};
+#endregion
+Console.Read();
+
---
+++ RabbitMQ.Message.Templates.Publisher/Program.cs
@@ -0,0 +1,113 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Connection initialize
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vbqelytc:MbF26kun1-5hNwzQ9Q8OQ--HckTNQuZu@shark.rmq.cloudamqp.com/vbqelytc");
+
+
+//Active connection and open channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+#region P2P, Point to Point design
+//string queueName = "Example-p2p-queue";
+
+//channel.QueueDeclare(
+//    queue: queueName,
+//    durable: false, 
+//    exclusive: false, 
+//    autoDelete: false);
+//byte[]message= Encoding.UTF8.GetBytes("Hello from P2p!");
+//channel.BasicPublish(
+//    exchange: string.Empty,
+//    routingKey: queueName,
+//    body: message);
+#endregion
+
+#region Pub/Sub, Publish/Subscribe design 
+//string exchangeName = "example-pub-sub-exchange";
+
+//channel.ExchangeDeclare(
+//    exchange: exchangeName,
+//    type: ExchangeType.Fanout);
+
+//byte[] message = Encoding.UTF8.GetBytes("Hello from Pub/Sub!");
+
+//channel.BasicPublish(
+//    exchange: exchangeName,
+//    routingKey: string.Empty,
+//    body: message);
+#endregion
+
+#region Work Queues design
+//string queueName = "example-work-queue";
+
+//channel.QueueDeclare(
+//    queue: queueName,
+//    durable: false,
+//    exclusive: false,
+//    autoDelete: false);
+
+//for(int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(1000);
+//    byte[] message = Encoding.UTF8.GetBytes($"Hello from Work Queues! {i}");
+
+//    channel.BasicPublish(
+//        exchange: string.Empty,
+//        routingKey: queueName,
+//        body: message);
+//}
+#endregion
+
+#region Request/Response design
+string requestQueueName = "example-request-response-queue";
+
+channel.QueueDeclare(
+    queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
+
+string replyQueueName = channel.QueueDeclare().QueueName;
+
+string correlationId = Guid.NewGuid().ToString();
+
+#region Create request message and sending
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.CorrelationId = correlationId;
+properties.ReplyTo = replyQueueName;
+
+for(int i = 0; i < 100; i++)
+{
+    byte[] message = Encoding.UTF8.GetBytes("hello"+i);
+
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: requestQueueName,
+        body: message,
+        basicProperties: properties);
+}
+#endregion
+
+#region Listening response queue
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: replyQueueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    if(eventArgs.BasicProperties.CorrelationId == correlationId)
+    {
+        Console.WriteLine($" Response : {Encoding.UTF8.GetString(eventArgs.Body.Span)}");
+    }
+};
+#endregion
+#endregion
+
+Console.Read();
+
