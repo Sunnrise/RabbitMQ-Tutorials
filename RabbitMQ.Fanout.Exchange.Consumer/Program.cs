0607ced5e74f3c3090e353f0173c7080da80e171
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-14 09:29:22 +0300

    Fanout.Exchange

---
+++ RabbitMQ.Fanout.Exchange.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+//Connection initialize
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vbqelytc:MbF26kun1-5hNwzQ9Q8OQ--HckTNQuZu@shark.rmq.cloudamqp.com/vbqelytc");
+
+
+//Active connection and open channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-rabbit",
+    type: ExchangeType.Fanout);
+
+Console.WriteLine("type a queue name");
+string queueName = Console.ReadLine();
+
+channel.QueueDeclare(
+    queue: queueName,
+    exclusive: false);
+channel.QueueBind(
+    queue: queueName,
+    exchange: "fanout-exchange-rabbit",
+    routingKey: string.Empty);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    string message = Encoding.UTF8.GetString(eventArgs.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.Read();
---
+++ RabbitMQ.Fanout.Exchange.Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Connection initialize
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vbqelytc:MbF26kun1-5hNwzQ9Q8OQ--HckTNQuZu@shark.rmq.cloudamqp.com/vbqelytc");
+
+
+//Active connection and open channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare("fanout-exchange-rabbit", ExchangeType.Fanout);
+
+for(int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message= Encoding.UTF8.GetBytes($"Zühremcim {i}");
+    channel.BasicPublish(
+        exchange:"fanout-exchange-rabbit", 
+        routingKey:string.Empty,
+        body: message);
+}
+Console.Read();
+
