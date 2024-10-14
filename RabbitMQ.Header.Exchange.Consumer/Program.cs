40f04dbc8a8af84163be4e0ffb9d161e10d1ff0b
Alperen Güneş <alperenmanavgat@outlook.com>
2024-10-14 10:20:43 +0300

    Header Exchange

---
+++ RabbitMQ.Header.Exchange.Consumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+//Connection initialize
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vbqelytc:MbF26kun1-5hNwzQ9Q8OQ--HckTNQuZu@shark.rmq.cloudamqp.com/vbqelytc");
+
+
+//Active connection and open channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "header-exchange-rabbit",
+    type: ExchangeType.Headers);
+
+Console.WriteLine("type a header value: ");
+string value = Console.ReadLine();
+string queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(
+    queue: queueName,
+    exchange: "header-exchange-rabbit",
+    routingKey: string.Empty,
+    new Dictionary<string, object>
+    {
+        { "no", value }
+    });
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine($"Received message: {message}");
+};
+
+Console.Read();
---
+++ RabbitMQ.Header.Exchange.Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Connection initialize
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://vbqelytc:MbF26kun1-5hNwzQ9Q8OQ--HckTNQuZu@shark.rmq.cloudamqp.com/vbqelytc");
+
+
+//Active connection and open channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare("header-exchange-rabbit", ExchangeType.Headers);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Message {i}");
+    Console.WriteLine("Type a Header Value: ");
+    string value = Console.ReadLine();
+
+    IBasicProperties basicProperties = channel.CreateBasicProperties();
+
+    basicProperties.Headers = new Dictionary<string, object>
+    {
+        { "no", value }
+    };
+
+    channel.BasicPublish(
+        exchange: "header-exchange-rabbit",
+        routingKey: string.Empty,
+        body: message,
+        basicProperties: basicProperties);
+}
+Console.Read();
+
